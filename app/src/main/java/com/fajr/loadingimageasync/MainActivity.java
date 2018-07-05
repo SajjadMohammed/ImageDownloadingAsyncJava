62804a05e631eaf71b6f8759a282f8ee7c9ca67a
SajjadMohammed <30750038+SajjadMohammed@users.noreply.github.com>
2018-07-05 12:05:10 +0300

    adding v7 toolbar

--- app/src/main/java/com/fajr/loadingimageasync/MainActivity.java
+++ app/src/main/java/com/fajr/loadingimageasync/MainActivity.java
@@ -2,6 +2,7 @@ package com.fajr.loadingimageasync;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
@@ -9,11 +10,15 @@ public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
     ProgressBar progressBar;
+    Toolbar V7ToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        V7ToolBar=findViewById(R.id.toolbar);
+        setSupportActionBar(V7ToolBar);
 
         imageView = findViewById(R.id.myImage);
         progressBar = findViewById(R.id.myProgressBar);
